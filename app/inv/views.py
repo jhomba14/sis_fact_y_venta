d374d42e0d0032bdcf737998dcd0c03317b237cd
Bairon <holguinbairon@hotmail.com>
2020-04-23 17:12:19 -0500

    Version con guardar categoria completa

--- app/inv/views.py
+++ app/inv/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Categoria
+from .forms import CategoriaForm
+from .forms import CategoriaForm
 
 # Create your views here.
 class CategoriaView(LoginRequiredMixin,generic.ListView):
@@ -11,3 +14,16 @@ class CategoriaView(LoginRequiredMixin,generic.ListView):
     template_name = "inv/categoria_list.html"
     context_object_name = "obj"
     login_url = 'bases:login'
+
+
+class CategoriaNew(LoginRequiredMixin, generic.CreateView):
+    model = Categoria
+    template_name = "inv/categoria_form.html"
+    context_object_name = "obj"
+    form_class = CategoriaForm
+    success_url = reverse_lazy('inv:categoria_list')
+    login_url = 'bases.login'
+
+    def form_valid(self, form):
+        form.instance.usuario_creador = self.request.user
+        return super().form_valid(form)
