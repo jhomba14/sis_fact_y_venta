2dd0b67ea59e0b9d543a939a16c587878441d994
Bairon <holguinbairon@hotmail.com>
2020-04-27 16:27:49 -0500

    Version con categoria completa

--- app/inv/views.py
+++ app/inv/views.py
@@ -27,3 +27,23 @@ class CategoriaNew(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         form.instance.usuario_creador = self.request.user
         return super().form_valid(form)
+
+
+class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
+    model = Categoria
+    template_name = "inv/categoria_form.html"
+    context_object_name = "obj"
+    form_class = CategoriaForm
+    success_url = reverse_lazy('inv:categoria_list')
+    login_url = 'bases.login'
+
+    def form_valid(self, form):
+        form.instance.usuario_modifica = self.request.user.id 
+        return super().form_valid(form)
+
+
+class CategoriaDel(LoginRequiredMixin, generic.DeleteView):
+    model = Categoria
+    template_name = "inv/catalogos_del.html"
+    context_object_name='obj'
+    success_url = reverse_lazy('inv:categoria_list')
